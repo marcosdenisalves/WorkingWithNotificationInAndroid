ca15cdaaf772bc694e3964c4ddef62bb52037a87
Denis Alves <dee.alves2@gmail.com>
2021-07-25 12:11:11 -0300

    Creating local notification in android

---
+++ app/src/main/java/com/myproject/appnotifications/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.myproject.appnotifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {
+            this.showNotification("1", "Look your notification my friend", "your received a notification made by a future android developer" )
+        }
+    }
+}
